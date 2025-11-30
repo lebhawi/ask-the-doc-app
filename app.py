c25e6532570474c117004da5d9bdfb959b3ba25b
lebhawi <56108111+lebhawi@users.noreply.github.com>
2025-11-29 17:29:46 -0800

    Refactor app.py for improved code structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 !pip install langchain==0.1.0
 import streamlit as st
 from langchain_openai import OpenAI
-#from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
@@ -45,6 +45,7 @@ with st.form('myform', clear_on_submit=True):
 
 if len(result):
     st.info(response)
+
 
 
 
